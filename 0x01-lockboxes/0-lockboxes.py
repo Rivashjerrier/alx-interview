a85e9d5196d44064bddc90b0034e43c7b6d91d8f
Rivashjerrier <rivijerrier@gmail.com>
2023-03-30 18:58:16 -0700

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,9 +6,9 @@ def canUnlockAll(boxes):
     """Determine if all boxes can be opened"""
     keys = [0]
     for key in keys:
-        for keyInBox in boxes[key]:
-            if keyInBox not in keys and keyInBoxey < len(boxes):
-                keys.append(keyInBox)
+        for box_with_key in boxes[key]:
+            if box_with_key not in keys and box_with_key < len(boxes):
+                keys.append(box_with_key)
     if len(keys) == len(boxes):
         return True
     return False
