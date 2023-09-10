2cf3e3af42e27e1bffb941c97d6e3fc967008f8b
Rivashjerrier <rivijerrier@gmail.com>
2023-09-10 23:45:44 +0000

    Prime Game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+0-prime_game.py module
+"""
+
+
+def isWinner(x, nums):
+    """
+    Returns name of the player that won the most rounds
+    """
+    maria = 0
+    ben = 0
+    for n in nums:
+        if n % 2 == 0 or is_prime(n):
+            ben += 1
+        else:
+            maria += 1
+
+    if maria < ben:
+        return "Ben"
+    elif maria > ben:
+        return "Maria"
+    else:
+        return None
+
+
+def is_prime(num):
+    """
+    Checks if a number is a prime number
+    """
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+        return True
